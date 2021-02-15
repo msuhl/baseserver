379c2f966f887575cffedbb5a3e4bed287da5d92
mjun <gate.nielsen@gmail.com>
2021-02-15 22:11:52 +0100

    Outcomment unnecessary functionality

--- src/Application.kt
+++ src/Application.kt
@@ -17,48 +17,50 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    install(Locations) {
-    }
+//    install(Locations) {
+//    }
 
-    install(ContentNegotiation) {
-        jackson {
-            enable(SerializationFeature.INDENT_OUTPUT)
-        }
-    }
+    install(CallLogging)
 
-    val client = HttpClient(Jetty) {
-    }
+//    install(ContentNegotiation) {
+//        jackson {
+//            enable(SerializationFeature.INDENT_OUTPUT)
+//        }
+//    }
+
+//    val client = HttpClient(Jetty) {
+//    }
 
     routing {
         get("/") {
-            call.respondText("HELLO mjuhl 2!", contentType = ContentType.Text.Plain)
+            call.respondText("Mjello mjuhl!", contentType = ContentType.Text.Plain)
         }
 
-        get<MyLocation> {
-            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
-        }
-        // Register nested routes
-        get<Type.Edit> {
-            call.respondText("Inside $it")
-        }
-        get<Type.List> {
-            call.respondText("Inside $it")
-        }
-
-        get("/json/jackson") {
-            call.respond(mapOf("hello" to "world"))
-        }
+//        get<MyLocation> {
+//            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
+//        }
+//        // Register nested routes
+//        get<Type.Edit> {
+//            call.respondText("Inside $it")
+//        }
+//        get<Type.List> {
+//            call.respondText("Inside $it")
+//        }
+//
+//        get("/json/jackson") {
+//            call.respond(mapOf("hello" to "world"))
+//        }
     }
 }
 
-@Location("/location/{name}")
-class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
-
-@Location("/type/{name}") data class Type(val name: String) {
-    @Location("/edit")
-    data class Edit(val type: Type)
-
-    @Location("/list/{page}")
-    data class List(val type: Type, val page: Int)
-}
+//@Location("/location/{name}")
+//class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
+//
+//@Location("/type/{name}") data class Type(val name: String) {
+//    @Location("/edit")
+//    data class Edit(val type: Type)
+//
+//    @Location("/list/{page}")
+//    data class List(val type: Type, val page: Int)
+//}
 
