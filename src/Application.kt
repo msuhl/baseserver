7fe0598ede026a9924065f0af775b74222ac46b4
mjun <gate.nielsen@gmail.com>
2021-02-16 08:12:20 +0100

    re-add netty and clean up

--- src/Application.kt
+++ src/Application.kt
@@ -12,56 +12,17 @@ import io.ktor.features.*
 import io.ktor.client.*
 import io.ktor.client.engine.jetty.*
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
 
-    @Suppress("unused") // Referenced in application.conf
-    fun Application.module(testing: Boolean = false) {
-//    install(Locations) {
-//    }
+    install(CallLogging)
 
-        install(CallLogging)
-
-//    install(ContentNegotiation) {
-//        jackson {
-//            enable(SerializationFeature.INDENT_OUTPUT)
-//        }
-//    }
-
-//    val client = HttpClient(Jetty) {
-//    }
-
-        routing {
-            get("/") {
-                call.respondText("Mjello mjuhl!", contentType = ContentType.Text.Plain)
-            }
-
-//        get<MyLocation> {
-//            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
-//        }
-//        // Register nested routes
-//        get<Type.Edit> {
-//            call.respondText("Inside $it")
-//        }
-//        get<Type.List> {
-//            call.respondText("Inside $it")
-//        }
-//
-//        get("/json/jackson") {
-//            call.respond(mapOf("hello" to "world"))
-//        }
+    routing {
+        get("/") {
+            call.respondText("Mjello mjuhl!", contentType = ContentType.Text.Plain)
         }
     }
 }
-
-//@Location("/location/{name}")
-//class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
-//
-//@Location("/type/{name}") data class Type(val name: String) {
-//    @Location("/edit")
-//    data class Edit(val type: Type)
-//
-//    @Location("/list/{page}")
-//    data class List(val type: Type, val page: Int)
-//}
-
