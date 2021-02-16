acd512ca62d748996a1873c00ddf74d0f85f7400
mjun <gate.nielsen@gmail.com>
2021-02-16 10:25:54 +0100

    Downgrade shadow

--- src/Application.kt
+++ src/Application.kt
@@ -17,12 +17,8 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-
+    install(DefaultHeaders)
     install(CallLogging)
-
-    install(CORS) {
-        anyHost()
-    }
 
     routing {
         get("/") {
